49d96cde96c4d250b024796e94aa5a3ae85306df
Rafael Almeida <rafael.almeida.soares2012@gmail.com>
2022-11-28 16:10:49 -0300

    create of interface of fragment home

---
+++ lib/pages/fragments/home_fragment.dart
@@ -0,0 +1,137 @@
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeFragment extends StatefulWidget {
+  const HomeFragment({Key? key}) : super(key: key);
+
+  @override
+  State<HomeFragment> createState() => _HomeFragmentState();
+}
+
+class _HomeFragmentState extends State<HomeFragment> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Image.asset('assets/images/background.png',
+                fit: BoxFit.cover, width: double.infinity, height: 200),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: CupertinoSearchTextField(
+                placeholder: 'Pesquisar',
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: CupertinoColors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Container(
+          height: 50,
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Card(
+                    child: ListTile(
+                      leading: Image.asset(
+                        'assets/images/background.png',
+                        width: 150,
+                        height: 150,
+                      ),
+                      title: Text('Nome do produto'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text('Descrição do produto'),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text('R\$ 10,00'),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Icon(
+                                  CupertinoIcons.star_fill,
+                                  color: CupertinoColors.systemYellow,
+                                  size: 15,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10, left: 10),
+                                child: Icon(
+                                  CupertinoIcons.star_fill,
+                                  color: CupertinoColors.systemYellow,
+                                  size: 15,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10, left: 10),
+                                child: Icon(
+                                  CupertinoIcons.star_fill,
+                                  color: CupertinoColors.systemYellow,
+                                  size: 15,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10, left: 10),
+                                child: Icon(
+                                  CupertinoIcons.star_lefthalf_fill,
+                                  color: CupertinoColors.systemYellow,
+                                  size: 15,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10, left: 10),
+                                child: Icon(
+                                  CupertinoIcons.star,
+                                  color: CupertinoColors.systemYellow,
+                                  size: 15,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10, left: 10),
+                                child: Text('3.5'),
+                              ),
+                            ],
+                          ),
+                          CupertinoButton(
+                            child: Text('Adicionar ao carrinho'),
+                            onPressed: () {},
+                            padding: const EdgeInsets.all(0),
+                          ),
+                        ],
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(CupertinoIcons.heart),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                );
+              }),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app/pages/fragments/home_fragment.dart';
 import 'package:flutter/cupertino.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,35 +10,34 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Widget> screens = [
-    const Text('History'),
-    const Text('Profile'),
-    const Text('Home'),
-    const Text('Profile'),
-    const Text('Cart'),
+    HomeFragment(),
+    const Text('Histórico'),
+    const Text('Perfil'),
+    const Text('Favoritos'),
+    const Text('Carrinho'),
   ];
-  
+
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
         tabBar: CupertinoTabBar(items: const [
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.text_badge_checkmark), label: 'History'),
+              icon: Icon(CupertinoIcons.home), label: 'Principal'),
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.person_circle), label: 'Profile'),
+              icon: Icon(CupertinoIcons.text_badge_checkmark),
+              label: 'Histórico'),
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home), label: 'Home'),
+              icon: Icon(CupertinoIcons.person_circle), label: 'Perfil'),
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.heart), label: 'Favorites'),
+              icon: Icon(CupertinoIcons.heart), label: 'Favoritos'),
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.bag), label: 'Cart'),
+              icon: Icon(CupertinoIcons.bag), label: 'Carrinho'),
         ]),
         tabBuilder: (context, index) {
           return CupertinoTabView(
             builder: (context) {
               return CupertinoPageScaffold(
-                child: SafeArea(
-                  child: screens[index],
-                ),
+                child: screens[index],
               );
             },
           );
