1e012bc1c2bf385b0ec8c0f816a08b7d382d3d28
Rafael Almeida <rafael.almeida.soares2012@gmail.com>
2022-12-02 21:19:21 -0300

    create controller of user

---
+++ lib/controllers/user_controller.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:delivery_app/models/user_model.dart';
+import 'package:flutter/cupertino.dart';
+
+class UserController {
+  final _colecao = FirebaseFirestore.instance.collection('users');
+
+  TextEditingController fieldTextUserName = TextEditingController();
+  TextEditingController fieldTextUserEmail = TextEditingController();
+  TextEditingController fieldTextUserPassword = TextEditingController();
+  TextEditingController fieldTextUserPhone = TextEditingController();
+  TextEditingController fieldTextUserAddress = TextEditingController();
+  TextEditingController fieldTextUserCard = TextEditingController();
+
+  Future<bool> createUser(String name, String email, String password,
+      String phone, String address, String card) async {
+    try {
+      UserModel user = UserModel(
+          name: name,
+          email: email,
+          password: password,
+          phone: phone,
+          address: address,
+          card: card);
+      await _colecao.doc(email).set(user.toJson());
+      _clearFields();
+      return true;
+    } catch (e) {
+      _clearFields();
+      return false;
+    }
+  }
+
+  Future<UserModel> getUser(String email) async {
+    var doc = await _colecao.doc(email).get();
+    return UserModel.fromJson(doc.data()!);
+  }
+
+  Future<bool> checkUser(String email, String password) async {
+    var doc = await _colecao.doc(email).get();
+    if(doc.exists){
+      return doc.data()!['password'] == password;
+    }
+    return false;
+  }
+
+  void _clearFields() {
+    fieldTextUserName.clear();
+    fieldTextUserEmail.clear();
+    fieldTextUserPassword.clear();
+    fieldTextUserPhone.clear();
+    fieldTextUserAddress.clear();
+    fieldTextUserCard.clear();
+  }
+}
