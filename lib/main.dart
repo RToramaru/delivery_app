597ef797a0ed132df8923b8036b0fe12d156c82c
Rafael Almeida <rafael.almeida.soares2012@gmail.com>
2022-12-02 21:18:47 -0300

    configure firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,9 @@ import 'package:delivery_app/pages/routes/app_route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 
-Future<void> main() async{
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    name: 'Delivery-app',
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const AppRoutes());
