15d31fcfc48fed75d8db3e51d4b176543291ebb8
Rafael Almeida <rafael.almeida.soares2012@gmail.com>
2022-12-07 13:53:06 -0300

    altered message modal

--- lib/pages/widgets/modal_widget.dart
+++ lib/pages/widgets/modal_widget.dart
@@ -4,8 +4,8 @@ void showModal(String title, String message, BuildContext context) {
   showCupertinoModalPopup<void>(
     context: context,
     builder: (BuildContext context) => CupertinoAlertDialog(
-      title: const Text('Erro!'),
-      content: const Text('Usuário ou senha inválidos!'),
+      title: Text(title),
+      content: Text(message),
       actions: <CupertinoDialogAction>[
         CupertinoDialogAction(
           child: const Text('Ok'),
